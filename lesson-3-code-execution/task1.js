e742128e97358fefa20dff57809da6fc76430db2
sergey <sergey.zatsialepin@gmail.com>
2022-11-02 19:03:57 +0300

    add task 1

---
+++ lesson-3-code-execution/task1.js
@@ -0,0 +1,22 @@
+'use strict'
+
+var tasksCompleted = {
+  'Anna': 29,
+  'Serg': 35,
+  'Elena': 1,
+  'Anton': 99
+};
+
+function salarySearch(argument) {
+  var key = '';
+  var meaning = 0;
+  for (var k in argument) {
+    if (meaning < argument[k]) {
+      meaning = argument[k];
+      key = k;
+    }
+  }
+  return meaning + ' ' + key;
+}
+
+console.log(salarySearch(tasksCompleted))
