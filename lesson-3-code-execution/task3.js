46378429e04cae6ff70a5a85d7e1ef66c02d42c9
sergey <sergey.zatsialepin@gmail.com>
2022-11-02 21:26:54 +0300

    add task3 v2.0

--- lesson-3-code-execution/task3.js
+++ lesson-3-code-execution/task3.js
@@ -9,7 +9,6 @@ do {
   }
 } while (isNaN(values) === false);
 
-/*
 function summa (argument) {
   var result = 0;
   for (var k = 0; k < argument.length; k++) {
@@ -19,10 +18,24 @@ function summa (argument) {
 }
 
 console.log(summa(valuesArray));
-*/
+
+/* version #2
+var valuesArray = [];
+
+var values = prompt('введите число');
+while (true) {
+  var valuesTrue = parseInt(values);
+  if (isNaN(valuesTrue)) {
+    break;
+  } else {
+    valuesArray.push(valuesTrue)
+    values = prompt('введите ещё');
+  }
+}
 
 var summ = valuesArray.reduce(function (result, item) {
   return result + item;
 });
 
 console.log(summ);
+*/
