f71b2013079c0cf486c53943a890d43bd3984bbc
sergey <sergey.zatsialepin@gmail.com>
2022-11-08 01:10:21 +0300

    fix task2

--- lesson-4-closures/task2.js
+++ lesson-4-closures/task2.js
@@ -1,21 +1,20 @@
 'use strict'
 
 var obj = {
-  name: 'serg andr ant egor ant'
+  className: 'my menu menu'
 };
 
-function fff(qqq, slovo) {
-  var mass = qqq.name.split(' ');
-  for (var k = 0; k < mass.length; k++) {
-    if (mass[k] === slovo) {
-      console.log(mass[k] + ' покинул чат');
-      mass.splice(k, 1);
+function removeClass(qqq, word) {
+  var massiv = qqq.className.split(' ');
+  for (var k = 0; k <= massiv.length; k++) {
+    if (massiv[k] === word) {
+      console.log(massiv[k] + ' покинул чат');
+      massiv.splice(k, 1);
+      k--;
     }
   }
-
-  obj.name = mass.join(' ');
-
+  obj.className = massiv.join(' ');
   return obj;
 }
-fff(obj,'ant');
+removeClass(obj,'menu');
 console.log(obj);
