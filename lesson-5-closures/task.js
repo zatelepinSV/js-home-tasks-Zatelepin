04f4dab46153cadcc88475d28979a0e12aefdae5
sergey <sergey.zatsialepin@gmail.com>
2022-11-16 02:00:59 +0300

    fix

--- lesson-5-closures/task.js
+++ lesson-5-closures/task.js
@@ -26,6 +26,81 @@ var john = {
       }
     }
   },
+};
+var mark = {
+  prices: [77, 375, 110, 45],
+  marksTips: [],
+  marksBillPlusTips: [],
+  pays: function (arg) {
+    var tips = 0;
+    var billAndTips = 0;
+    for (var k = 0; k < arg.length; k++) {
+      if (arg[k] < 100) {
+        tips = arg[k] * 20 / 100;
+        billAndTips = arg[k] + tips;
+        this.marksTips.push(tips);
+        this.marksBillPlusTips.push(billAndTips);
+      } else if (arg[k] > 100 && arg[k] < 300) {
+        tips = arg[k] * 10 / 100;
+        this.marksTips.push(tips);
+        billAndTips = arg[k] + tips;
+        this.marksBillPlusTips.push(billAndTips);
+      } else {
+        tips = arg[k] * 25 / 100;
+        billAndTips = arg[k] + tips;
+        this.marksTips.push(tips);
+        this.marksBillPlusTips.push(billAndTips);
+      }
+    }
+  },
+};
+
+function average (arg) {
+  var average;
+  var result = 0;
+  for (var k = 0; k < arg.length; k++) {
+    result = arg[k] + result;
+    average = k;
+  }
+  result /= (average + 1);
+  return result;
+}
+john.pays(john.prices);
+mark.pays(mark.prices);
+
+if (average(john.johnsTips) > average(mark.marksTips)) {
+  console.log('Марк просадил больше');
+} else {
+  console.log('Джон просадил больше');
+}
+
+/* Version #2
+var john = {
+  prices: [124, 48, 268, 180, 42],
+  johnsTips: [],
+  johnsBillPlusTips: [],
+  pays: function (arg) {
+    var tips = 0;
+    var billAndTips = 0;
+    for (var k = 0; k < arg.length; k++) {
+      if (arg[k] < 50) {
+        tips = arg[k] * 20 / 100;
+        billAndTips = arg[k] + tips;
+        this.johnsTips.push(tips);
+        this.johnsBillPlusTips.push(billAndTips);
+      } else if (arg[k] > 50 && arg[k] < 200) {
+        tips = arg[k] * 15 / 100;
+        this.johnsTips.push(tips);
+        billAndTips = arg[k] + tips;
+        this.johnsBillPlusTips.push(billAndTips);
+      } else {
+        tips = arg[k] * 10 / 100;
+        billAndTips = arg[k] + tips;
+        this.johnsTips.push(tips);
+        this.johnsBillPlusTips.push(billAndTips);
+      }
+    }
+  },
   average: function (arg) {
     john.pays(john.prices);
     var average;
@@ -79,7 +154,8 @@ var mark = {
 };
 
 if (mark.average(mark.marksTips) > john.average(john.johnsTips)) {
-  console.log('Марк просадил больше ' + mark.average(mark.marksTips));
+  console.log('Марк просадил больше ');
 } else {
-  console.log('Джон просадил больше ' + john.average(john.johnsTips));
+  console.log('Джон просадил больше ');
 }
+*/
