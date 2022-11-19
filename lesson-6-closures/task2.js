343d70b883ce83320287df4966c7c521df984bbb
sergey <sergey.zatsialepin@gmail.com>
2022-11-20 00:44:59 +0300

    fix variable name

--- lesson-6-closures/task2.js
+++ lesson-6-closures/task2.js
@@ -1,15 +1,16 @@
 'use strict'
 
-function interviewQuestion(prof) {
-  if (prof === 'designer') {
+
+function interviewQuestion(job) {
+  if (job === 'designer') {
     return function (name) {
       return (name + ' can you please explain what UX design is?');
     };
-  } else if (prof === 'teacher') {
+  } else if (job === 'teacher') {
     return function (name) {
       return ('What subject do you teach ' + name + '?');
     };
-  } else if (prof === 'other') {
+  } else if (job === 'other') {
     return function (name) {
       return ('Hello ' + name + ', what do you do?');
     };
