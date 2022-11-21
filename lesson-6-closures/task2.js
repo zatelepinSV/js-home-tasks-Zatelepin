1f4fc9ae0a76ab994df7df9bf8ec37ac2f81ec47
sergey <sergey.zatsialepin@gmail.com>
2022-11-21 14:01:23 +0300

    add task2 vers2

--- lesson-6-closures/task2.js
+++ lesson-6-closures/task2.js
@@ -2,6 +2,23 @@
 
 
 function interviewQuestion(job) {
+  return function (name) {
+    if (job === 'designer') {
+      return (name + ' can you please explain what UX design is?');
+    } else if (job === 'teacher') {
+      return ('What subject do you teach ' + name + '?');
+    } else if (job === 'other') {
+      return ('Hello ' + name + ', what do you do?');
+    }
+  };
+}
+
+console.log(interviewQuestion('teacher')('John'));
+
+/* version #2 (решил, что лучше задекларировать 1 ф-цию и там уже эта функция решает что делать,
+чем декларировать 3 функции которые делают тоже самое)
+
+function interviewQuestion(job) {
   if (job === 'designer') {
     return function (name) {
       return (name + ' can you please explain what UX design is?');
@@ -18,3 +35,4 @@ function interviewQuestion(job) {
 }
 
 console.log(interviewQuestion('designer')('John'));
+*/
